dc69aefc52fd6e8da89d9320e123bdf9ec5bd663
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-11-15 22:14:14 +0600

    stream and buffer

---
+++ stream-buffer.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require("fs")
+const { buffer } = require('stream/consumers')
+
+// creating a server using raw nodejs
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+    if(req.url === "/read-file" && req.method === "GET"){
+
+    // streaming file reading
+    const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt')
+    readableStream.on("data", (buffer) => {
+        res.statusCode = 200
+        res.write(buffer)
+    })
+    readableStream.on('end', () => {
+        res.statusCode = 200
+    res.end('the streaming is over')
+    })
+
+    readableStream.on('error', (error) => {
+        console.log(error)
+        res.statusCode = 500
+        res.end('something went wrong')
+        })
+
+
+    }
+    })
+
+server.listen(5000, ()=>{
+    console.log(`server is listening on port 5000`)
+})
